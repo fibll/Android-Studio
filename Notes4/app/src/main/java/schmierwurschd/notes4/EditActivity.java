29c02beab0461ed1c43644fb88f52101c719761d
fibll <acc.simonretsch@icloud.com>
2017-10-24 20:13:01 +0200

    ADD editActivity write into items file ADD mainActivity read out of items file and initialize list with 1 item

--- Notes4/app/src/main/java/schmierwurschd/notes4/EditActivity.java
+++ Notes4/app/src/main/java/schmierwurschd/notes4/EditActivity.java
@@ -4,6 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -11,11 +19,32 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
     }
 
     public void saveNote(View view) {
         // create intent to main activity
         Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextField);
+
+        // write item into item file
+        try {
+            // create output stream and output writer
+            FileOutputStream fileout = openFileOutput("items.txt", MODE_PRIVATE);
+            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+
+            // write into file
+            outputWriter.write(editText.getText().toString());
+
+            // close file
+            outputWriter.close();
+
+            //display file saved message
+            //Toast.makeText(getBaseContext(), "File saved successfully!", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // jump to main activity
         startActivity(intent);
--- Notes4/app/src/main/java/schmierwurschd/notes4/MainActivity.java
+++ Notes4/app/src/main/java/schmierwurschd/notes4/MainActivity.java
@@ -4,13 +4,66 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // variables
+    //  List
+    private ArrayList<String> listItems = new ArrayList<String>();
+    private ArrayAdapter adapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //-----
+
+        // initialize List
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
+
+        // get list items out of list file
+        try {
+            // string that contains characters of file
+            String items = "";
+
+            // file input stream and stream reader
+            FileInputStream fileIn = openFileInput("items.txt");
+            InputStreamReader InputRead = new InputStreamReader(fileIn);
+
+            // create input buffer and read char counter
+            char[] inputBuffer = new char[100];
+            int charRead;
+
+            // read out of file
+            while ((charRead = InputRead.read(inputBuffer)) > 0) {
+
+                // char to string conversion
+                String readstring = String.copyValueOf(inputBuffer, 0, charRead);
+                items += readstring;
+            }
+            InputRead.close();
+
+            // add items to list
+            // TO CHANGE
+            listItems.add(items);
+
+            //display data of items.txt
+            // Toast.makeText(getBaseContext(), items, Toast.LENGTH_SHORT).show();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void newNote(View view) {
